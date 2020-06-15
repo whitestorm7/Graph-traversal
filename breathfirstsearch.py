bbcda4515f3f70150c571e3219b3bb603bce0dd8
whitestorm7 <66906974+whitestorm7@users.noreply.github.com>
2020-06-15 21:40:58 +0530

    Update breathfirstsearch.py

--- breathfirstsearch.py
+++ breathfirstsearch.py
@@ -1,17 +1,17 @@
 class Graph:
-    def __init__(self,nodes,edge):
-        self.__nodes=nodes
+    def __init__(self,edge):
+       
         self.__edge=edge
         self.__adjlist={}
     def add_adj_list(self):
-        for i in nodes:
-            k=[]
-            self.__adjlist[i]=k
+        
         for i in edge:
+            if i[0] not in self.__adjlist:
+                self.__adjlist[i[0]]=[]
+            if i[1] not in self.__adjlist:
+                self.__adjlist[i[1]]=[]
             self.__adjlist[i[0]].append(i[1])
-           
-            self.__adjlist[i[1]].append(i[0])
-            
+            self.__adjlist[i[1]].append(i[0])       
     def print_adj_list(self):
         for i in self.__adjlist:
             print(i,end=" ")
@@ -32,7 +32,7 @@ class Graph:
             start=stack[i]
             
         print(stack)
-nodes=[1,2,3,4,5,6,7,8,9,10]
+
 edge=[[1,4],[1,2],[4,3],[3,10],[3,9],[3,2],[2,5],[2,7],[2,8],[5,7],[5,6],[5,8],[8,7]]
 g=Graph(nodes,edge)    
 g.add_adj_list()
